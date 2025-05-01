d69c2e27eb20aa9b29fe4fb92ee645d3d5f45fcf
realkoder <alexanderbtcc@gmail.com>
2025-05-01 23:08:47 +0200

    fixed the db con url

--- backend/baskets/src/db/database.ts
+++ backend/baskets/src/db/database.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prismaBaskets = new PrismaClient({
   datasources: {
     db: {
-      url: "postgresql://postgres:admin@postgres:5432/baskets",
+      url: process.env.ENV === "docker-compose" ? "postgresql://postgres:admin@postgresdb:5432/baskets" : "postgresql://postgres:admin@localhost:5431/baskets",
     },
   },
 });
--- backend/products/src/db/database.ts
+++ backend/products/src/db/database.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prismaProducts = new PrismaClient({
   datasources: {
     db: {
-      url: "postgresql://postgres:admin@postgres:5432/products",
+      url: process.env.ENV === "docker-compose" ? "postgresql://postgres:admin@postgresdb:5432/products" : "postgresql://postgres:admin@localhost:5431/products",
     },
   },
 });
