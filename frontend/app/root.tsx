20e30337200652bed80ed0dea72ba836a8f0fe3b
realkoder <alexanderbtcc@gmail.com>
2025-06-12 11:45:50 +0200

    fixed issue with favicon

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -48,6 +48,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="favicon.ico?v=2" />
+
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
